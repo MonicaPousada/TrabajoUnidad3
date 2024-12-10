1318383a61772e56a7e300e9be443aa8107cb185
monicapousada <monicapousada300@gmail.com>
2024-12-10 17:23:14 +0100

    commit & push inicial de la rama "tarea5"

--- app/src/main/java/com/example/trabajounidad3/vista/MainActivity.java
+++ app/src/main/java/com/example/trabajounidad3/vista/MainActivity.java
@@ -34,7 +34,7 @@ import com.example.trabajounidad3.utils.Adaptador;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+//coment
     private Switch arriba;
     private Switch abajo;
     private Switch zapatos;
